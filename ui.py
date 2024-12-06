057c11b9aa07198a7c792cbb953e2438c07b35b0
gkgkdml91 <les7260@naver.com>
2024-12-06 10:26:00 +0900

    Revert "Add lbl1 in ui.py to print current date"
    This reverts commit e17ce4470c298147c169410511d6702143048a19.

--- ui.py
+++ ui.py
@@ -1,34 +1,27 @@
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout,
-                             QMessageBox, QPlainTextEdit, QHBoxLayout, QLabel)
+from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
+                             QMessageBox, QPlainTextEdit, QHBoxLayout)
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QDate, Qt
+
 
 class View(QWidget):
     
     def __init__(self):
         super().__init__()
-        self.date = QDate.currentDate()
         self.initUI()
         
     def initUI(self):
-        self.lbl1 = QLabel(self.date.toString(Qt.DefaultLocaleLongDate), self)
         self.te1 = QPlainTextEdit()
         self.te1.setReadOnly(True)
         
-        self.btn1 = QPushButton('Message', self)
-        self.btn2 = QPushButton('Clear', self)
-        
-        # 버튼 클릭 이벤트 연결
-        self.btn1.clicked.connect(self.activateMessage)
-        self.btn2.clicked.connect(self.clearMessage)
+        self.btn1=QPushButton('Message',self)
+        self.btn2=QPushButton('Clear',self)
         
         hbox = QHBoxLayout()
         hbox.addStretch(1)
         hbox.addWidget(self.btn1)
         hbox.addWidget(self.btn2)
         
-        vbox = QVBoxLayout()
-        vbox.addWidget(self.lbl1)  # QLabel 추가
+        vbox=QVBoxLayout()
         vbox.addWidget(self.te1)
         vbox.addLayout(hbox)
         vbox.addStretch(1)
@@ -37,7 +30,7 @@ class View(QWidget):
         
         self.setWindowTitle('Calculator')
         self.setWindowIcon(QIcon('icon.png'))
-        self.resize(256, 256)
+        self.resize(256,256)
         self.show()
         
     def activateMessage(self):
@@ -45,9 +38,3 @@ class View(QWidget):
         
     def clearMessage(self):
         self.te1.clear()
-
-if __name__ == '__main__':
-    import sys
-    app = QApplication(sys.argv)
-    view = View()
-    sys.exit(app.exec_())
